ca396757f18a15a12db8a3f68087efe19fd0cf51
Karin <samoviskik@gmail.com>
2020-09-25 12:50:46 -0400

    update to shelter test

--- src/test/java/shelter/ShelterTest.java
+++ src/test/java/shelter/ShelterTest.java
@@ -7,16 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ShelterTest {
 
     Shelter underTest = new Shelter();
-    OrganicPet dog = new OrganicDog("Archer", "Lazy",  50, 50,50,50);
-    OrganicCat cat = new OrganicCat("Fluffy", "Fat", 50, 50,50;
+    OrganicPet organicDog = new OrganicDog("Archer", "Lazy",  50, 50,50,50);
+    OrganicCat organicCat = new OrganicCat("Fluffy", "Fat", 50, 50,50);
     RoboDog roboDog = new RoboDog("Dino", "Sassy", 50, 50);
     RoboCat roboCat = new RoboCat("Fred", "Smart", 50, 50);
 
-
     @Test
-    public void shouldBeAbleToAddAnOrganicDog(){
+    public void shouldBeAbleToHireADoctor(){
         underTest.add(organicDog);
         OrganicPet foundOrganicDog = underTest.findOrganicPet("Archer");
-        assertEquals("Archer", foundOrganicDog.getName());
+        assertEquals("organicDog name", foundOrganicDog.getName());
     }
+
 }
+
+
+
