d26829c89eb753cf4335f38971ca86a4815c6dab
Karin <samoviskik@gmail.com>
2020-09-25 15:59:44 -0400

    remove test not working

--- src/main/java/shelter/Shelter.java
+++ src/main/java/shelter/Shelter.java
@@ -1,6 +1,8 @@
 package shelter;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,4 +18,35 @@ public class Shelter {
     public ShelterPet findPet(String name) {
         return (ShelterPet) petList.get(name);
     }
+
+    public Collection<ShelterPet> getAllPets() {
+        return petList.values();
+    }
+
+    public Collection<ShelterPet> getOrganicPets() {
+        Collection<ShelterPet> organicPets = new ArrayList<>();
+        for (ShelterPet pet : petList.values()) {
+            if (pet instanceof OrganicPet) {
+                organicPets.add(pet);
+            }
+        }
+        return organicPets;
+    }
+
+    public Collection<ShelterPet> getRoboPets() {
+        Collection<ShelterPet> roboPets = new ArrayList<>();
+        for (ShelterPet pet : petList.values()){
+            if (pet instanceof RoboPet){
+                roboPets.add(pet);
+            }
+        }
+        return roboPets;
+    }
+
+    public void remove(ShelterPet shelterPetToRemove) {
+        petList.remove(shelterPetToRemove.getName(), shelterPetToRemove);
+
+    }
+
 }
+
--- src/test/java/shelter/ShelterTest.java
+++ src/test/java/shelter/ShelterTest.java
@@ -2,6 +2,10 @@ package shelter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShelterTest {
@@ -27,7 +31,7 @@ public class ShelterTest {
         ShelterPet foundOrganicDog = underTest.findPet("Archer");
         ShelterPet foundOrganicCat = underTest.findPet("Fluffy");
         assertEquals("Archer", foundOrganicDog.getName());
-        assertEquals("Fluffy", foundOrganicCat);
+        assertEquals("Fluffy", foundOrganicCat.getName());
     }
 
     @Test
@@ -40,7 +44,47 @@ public class ShelterTest {
         assertEquals("Fred", foundRoboCat.getName());
     }
 
+    @Test
+    public void shouldBeAbleToAddACollectionOfPets(){
+        underTest.add(organicDog);
+        underTest.add(organicCat);
+        underTest.add(roboDog);
+        underTest.add(roboCat);
+        Collection<ShelterPet> addedPets = underTest.getAllPets();
+        assertThat(addedPets, containsInAnyOrder(organicDog,organicCat,roboCat,roboDog));
     }
+
+    @Test
+    public void shouldBeAbleToDisplayAllOrganicPets(){
+        underTest.add(organicDog);
+        underTest.add(organicCat);
+        underTest.add(roboDog);
+        underTest.add(roboCat);
+        Collection<ShelterPet> organicPets = underTest.getOrganicPets();
+        assertThat(organicPets, containsInAnyOrder(organicCat, organicDog));
+    }
+
+    @Test
+    public void shouldBeAbleToDisplayAllRoboPets(){
+        underTest.add(organicDog);
+        underTest.add(organicCat);
+        underTest.add(roboDog);
+        underTest.add(roboCat);
+        Collection<ShelterPet> roboPets = underTest.getRoboPets();
+        assertThat(roboPets, containsInAnyOrder(roboCat, roboDog));
+    }
+
+    @Test
+    public void shouldBeAbleToRemoveAnOrganicDog(){
+        underTest.remove(organicDog);
+        ShelterPet foundOrganicDog = underTest.findPet("Archer");
+        assertEquals("Archer", foundOrganicDog.getName());
+
+    }
+
+
+
+}
 
 
 
